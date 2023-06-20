5edab31bb77d326b3f50b6ea73f0a21caf0c0354
Z1241377627 <59963389+Z1241377627@users.noreply.github.com>
2023-06-20 14:35:03 +0800

    1

---
+++ src/main/java/com/zch/service/GradeService.java
@@ -0,0 +1,24 @@
+package com.zch.service;
+
+import com.zch.po.Grade;
+
+import java.util.List;
+
+public interface GradeService {
+	
+	/**
+     * 	 添加成绩
+     * @param grade   成绩
+     * @return  插入结果 !=0则插入成功
+     */
+    public int insertGrade(Grade grade);
+    /**
+     * 	查询学生自己的总学分
+     */
+    public String queryCreditsSum(String sid);
+    /**
+     * 	学生查看本人已修改课程
+     */
+    public List<Grade> getEedCourseBySid(int pageNo, int pageSize, String sid);
+
+}
---
+++ src/main/java/com/zch/service/impl/GradeServiceImpl.java
@@ -0,0 +1,40 @@
+package com.zch.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.zch.mapper.GradeMapper;
+import com.zch.po.Grade;
+import com.zch.service.GradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class GradeServiceImpl implements GradeService {
+
+	@Autowired
+	private GradeMapper gradeMapper;
+	
+	@Override
+	public int insertGrade(Grade grade) {
+		
+		return gradeMapper.insertGrade(grade);
+	}
+
+	@Override
+	public String queryCreditsSum(String sid) {
+		return gradeMapper.queryCreditsSum(sid);
+	}
+
+	@Override
+	public List<Grade> getEedCourseBySid(int pageNo, int pageSize, String sid) {
+		Map<String,Object> data = new HashMap<String,Object>();
+        data.put("pageNo",(pageNo-1) * pageSize);
+        data.put("pageSize",pageSize);
+        data.put("sid",sid);
+        return gradeMapper.getEedCourseBySid(data);
+	}
+
+}
